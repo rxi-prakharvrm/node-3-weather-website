eacf17ed9d4e22fd0d58eeb864841eb4105f72eb
Prakhar Verma <prakharverma1022@gmail.com>
2021-08-24 08:55:11 +0530

    Style input field and submit button and trigger submit button on pressing Enter key

--- public/js/app.js
+++ public/js/app.js
@@ -21,3 +21,13 @@ submitBtn.addEventListener("click", (e) => {
         })
     })
 })
+
+address.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      submitBtn.click();
+    }
+  });
